98f52108c8051340d3409592adf53c41f985258b
Wilson Wong <33595101+wongwi24@users.noreply.github.com>
2021-03-17 18:34:03 +0800

    Add Naive Bayes and KNearest Neighbors Classifier
    Update ClassicalML.py with Naive Bayes and KNearest Neighbors Classifier

--- Classical_ML.py
+++ Classical_ML.py
@@ -3,7 +3,7 @@
 
 # ## Import Libraries
 
-# In[5]:
+# In[1]:
 
 
 import numpy as np
@@ -14,7 +14,7 @@ import seaborn as sb
 
 # ## Import and Analysis of data
 
-# In[6]:
+# In[2]:
 
 
 card_data = pd.read_csv('creditcard.csv\creditcard.csv')
@@ -23,20 +23,20 @@ X = card_data.iloc[:, :-1]
 Y = card_data.iloc[:, -1]
 
 
-# In[7]:
+# In[3]:
 
 
 card_data.describe()
 
 
-# In[8]:
+# In[4]:
 
 
 # Check if there is null values
 card_data.isnull().sum()
 
 
-# In[9]:
+# In[5]:
 
 
 #Plot Fraud vs Not Fraud transaction counts
@@ -48,7 +48,7 @@ plt.xticks([0,1], labels=["not fraud","fraud"])
 plt.show()
 
 
-# In[10]:
+# In[6]:
 
 
 #Plot features correlation
@@ -59,7 +59,7 @@ sb.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns)
 plt.show
 
 
-# In[11]:
+# In[7]:
 
 
 #Transaction Time density plot
@@ -72,14 +72,14 @@ plt.legend()
 plt.show()
 
 
-# In[12]:
+# In[8]:
 
 
 sb.boxplot(x = "Class", y = "Amount", hue = "Class", data = card_data, showfliers = False)
 plt.show()
 
 
-# In[13]:
+# In[9]:
 
 
 #Features density plot
@@ -102,7 +102,7 @@ plt.show()
 
 # ## Data Preprocessing
 
-# In[14]:
+# In[10]:
 
 
 #Split dataset into test train and valid
@@ -113,7 +113,7 @@ x_train_v, x_test, y_train_v, y_test = train_test_split(X, Y, stratify = Y, test
 x_train, x_valid, y_train, y_valid = train_test_split(x_train_v, y_train_v, stratify = y_train_v, test_size = 0.25, random_state = 5)
 
 
-# In[15]:
+# In[11]:
 
 
 sc = StandardScaler()
@@ -122,7 +122,7 @@ x_test = sc.fit_transform(x_test)
 x_valid = sc.fit_transform(x_valid)
 
 
-# In[16]:
+# In[12]:
 
 
 weight_nf = y_train.value_counts()[0] / len(y_train)
@@ -131,7 +131,7 @@ print(f"Non-Fraud weight: {weight_nf}")
 print(f"Fraud weight: {weight_f}")
 
 
-# In[17]:
+# In[13]:
 
 
 print(f"Train Data shape: {x_train.shape} Train Class Data shape: {y_train.shape}")
@@ -141,16 +141,16 @@ print(f"Valid Data shape: {x_valid.shape} Valid Class Data shape: {y_valid.shape
 
 # ## Random Forest Classifier
 
-# In[18]:
+# In[14]:
 
 
 from sklearn.ensemble import RandomForestClassifier
-randfclassifier = RandomForestClassifier(n_estimators = 10, criterion = "entropy")
+randfclassifier = RandomForestClassifier(n_estimators = 100, criterion = "entropy")
 randfclassifier.fit(x_train, y_train)
 y_valid_pred_randf = randfclassifier.predict(x_valid)
 
 
-# In[19]:
+# In[15]:
 
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, f1_score, roc_auc_score
@@ -160,23 +160,23 @@ def print_classification_result(true, predict):
     print(f"ROC_AUC_Score:{roc_auc_score(true, predict)}")
 
 
-# In[20]:
+# In[16]:
 
 
 print_classification_result(y_valid, y_valid_pred_randf)
 
 
-# In[24]:
+# In[17]:
 
 
-#Change number of trees in forest to 100
-randfclassifier2 = RandomForestClassifier(n_estimators = 100, criterion = "entropy")
+#Change number of trees in forest to 200
+randfclassifier2 = RandomForestClassifier(n_estimators = 200, criterion = "entropy")
 randfclassifier2.fit(x_train, y_train)
 y_valid_pred_randf = randfclassifier2.predict(x_valid)
 print_classification_result(y_valid, y_valid_pred_randf)
 
 
-# In[22]:
+# In[18]:
 
 
 y_test_pred_randf = randfclassifier2.predict(x_test)
@@ -185,14 +185,55 @@ print_classification_result(y_test, y_test_pred_randf)
 
 # ## Kernel Support Vector Machine
 
-# In[23]:
+# In[19]:
 
 
 from sklearn.svm import SVC
 KSVM = SVC(kernel = "rbf")
 KSVM.fit(x_train, y_train)
-y_valid_pred_KSVM = KSVM.predict(x_valid)
-print_classification_result(y_valid, y_valid_pred_KSVM)
+y_test_pred_KSVM = KSVM.predict(x_test)
+print_classification_result(y_test, y_test_pred_KSVM)
+
+
+# ## K Nearest Neighbors
+
+# In[20]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 6, p = 2, metric = "minkowski")
+knn.fit(x_train, y_train)
+y_valid_pred_KNN = knn.predict(x_valid)
+print_classification_result(y_valid, y_valid_pred_KNN)
+
+
+# In[21]:
+
+
+#Change number of neighbors to 5
+knn2 = KNeighborsClassifier(n_neighbors = 5, p = 2, metric = "minkowski")
+knn2.fit(x_train, y_train)
+y_valid_pred_KNN = knn2.predict(x_valid)
+print_classification_result(y_valid, y_valid_pred_KNN)
+
+
+# In[22]:
+
+
+y_test_pred_KNN = knn2.predict(x_test)
+print_classification_result(y_test, y_test_pred_KNN)
+
+
+# ## Naive Bayes Classifier
+
+# In[23]:
+
+
+from sklearn.naive_bayes import GaussianNB
+nb = GaussianNB()
+nb.fit(x_train, y_train)
+y_test_pred_NB = nb.predict(x_test)
+print_classification_result(y_test, y_test_pred_NB)
 
 
 # In[ ]:
